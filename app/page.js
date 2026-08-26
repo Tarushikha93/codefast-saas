bd1706ad34f79ef10bf7993f521980c450caddd1
Tarushikha Sharma <shikha.taru93@gmail.com>
2026-08-26 13:41:08 +0530

    Update homepage feedback messaging

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn btn-primary">Let's go</button>
+      <h1> collect customer feedback and improve your business</h1>
+      <div>
+        create a feedback board in minuted, prioritize feature requests, and
+        keep your customers in the loop.
+      </div>
     </main>
   );
 }
